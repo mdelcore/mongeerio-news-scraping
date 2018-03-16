86beaf82a587ac93ee5d7e9e9adc330bd55f8095
Michael Delcore <nmdelcore@gmail.com>
2018-03-16 07:56:43 -0600

    updated deployed database info

--- server.js
+++ server.js
@@ -20,25 +20,25 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("./public"));
 
 // connect to database
-// mongoose.Promise = Promise;
-// var dbConnect = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadLines";
-// if(process.env.MONGODB_URI) {
-//     mongoose.connect(process.env.MONGODB_URI)
-// } else {
-//     mongoose.connect(dbConnect);
-// }
-
-// // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-// // Set mongoose to leverage built in JavaScript ES6 Promises
-// // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI, {
-  useMongoClient: true
-});
-
-
+var dbConnect = process.env.MONGODB_URI || "mongodb://localhost/heroku_zm9m8h1g";
+if(process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI)
+} else {
+    mongoose.connect(dbConnect);
+}
+// mongodb://foxsScrape:password12@ds119585.mlab.com:19585/heroku_hd8909ql;
+// Connect mongoose to our database
+/* mongoose.connect(dbConnect, function (error) {
+    // Log any errors connecting with mongoose
+    if (error) {
+        console.log(error);
+    }
+    // Or log a success message
+    else {
+        console.log("Mongoose connection is successful");
+    }
+}); */
 var db = mongoose.connection;
 db.on('error',function(err){
     console.log('Mongoose Error',err);
